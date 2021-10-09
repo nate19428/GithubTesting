b3f9d5f765591ab55af96009d5cb332b60e1a3b1
nate19428 <nicholaslchang@gmail.com>
2021-10-09 12:53:05 -0700

    starting code

---
+++ Test/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main (String args[]){
+        System.out.println("Hello World");
+        System.out.println("aksjfklajslkf");
+        System.out.println("AJFKkfkj");
+
+        Thing newthing = new Thing("ball");
+    }
+}
---
+++ Test/Thing.java
@@ -0,0 +1,6 @@
+public class Thing {
+    private String name;
+    public Thing(String name){
+        this.name = name;
+    }
+}
